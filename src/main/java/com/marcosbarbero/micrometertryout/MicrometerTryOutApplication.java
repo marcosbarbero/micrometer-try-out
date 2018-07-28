32822ff3cc0eb1cd5a33014725e0d73d2d60766e
Marcos Barbero <marcos.hgb@gmail.com>
2018-07-28 22:49:18 +0200

    Initial commit

---
+++ src/main/java/com/marcosbarbero/micrometertryout/MicrometerTryOutApplication.java
@@ -0,0 +1,12 @@
+package com.marcosbarbero.micrometertryout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerTryOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrometerTryOutApplication.class, args);
+	}
+}
